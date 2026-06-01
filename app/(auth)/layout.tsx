6c2816c9c73a4d955647a7cff0bf7fca29ad467b
Aaditya <aaditya@happy.hello.com>
2026-06-01 22:50:59 +0530

    Added the sign-in sign-up functionalities

--- app/(auth)/layout.tsx
+++ app/(auth)/layout.tsx
@@ -1,6 +1,23 @@
 import React, { ReactNode } from "react";
+import Image from "next/image";
+import SocialAuthForm from "@/components/forms/SocialAuthForm";
 
 const AuthLayout = ({ children }: { children: ReactNode }) => {
-  return <main className="bg-auth-light dark:bg-auth-dark">{children}</main>;
+  return (<main className="bg-auth-light flex dark:bg-auth-dark justify-center items-center bg-cover min-h-screen bg-no-repeat px-4 py-10">
+    
+   <section className="light-border background-light800_dark200 shadow-light100_dark100 min-w-full rounded-[10px] border px-4 py-10 shadow-md sm:min-w-[520px] sm:px-8">
+    <div className = "flex items-center justify-between gap-2">
+      <div className="space-y-2.5">
+        <h1 className="h2-bold text-dark100_light900"> Join Devflow</h1>
+        <p className="paragraph-regular text-dark500_light400"> To get your questions answered</p>
+
+      </div>
+      <Image src = "images/site-logo.svg" alt="dev Flow logo" width={50} height={50}className="object-contain"></Image>
+    </div>
+    {children}
+    <p>Social Auth</p>
+    <SocialAuthForm/ >
+    </section>
+  </main>);
 };
 export default AuthLayout;
--- app/(auth)/sign-in/page.tsx
+++ app/(auth)/sign-in/page.tsx
@@ -2,6 +2,6 @@ import AuthCard from "@/components/auth/authCard";
 import React from "react";
 
 const SignIn = () => {
-  return <AuthCard cardName="Create your account" cardDesc="to continue to DevFlow"></AuthCard>;
+  return <div>Sign IN</div>
 };
 export default SignIn;
